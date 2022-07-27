ab9226389b823583b2aca8fd6d5b5f1032cf939b
Murithijoshua <brodiamyzalius@gmail.com>
2022-07-27 04:30:06 +0300

    working on new feedback changes

--- resources/views/componets/hero.blade.php
+++ resources/views/componets/hero.blade.php
@@ -2,12 +2,14 @@
 <div class="w-full h-1/2 shadow-md bg-cover rounded-sm bg-gray-100 dark:bg-transparent bg-center rounded-md" style="height:32rem;">
     <div class="flex items-center justify-center h-full w-full bg-gray-10 bg-opacity-0 dark:bg-transparent">
         <div class="text-center">
-            <h1 class="text-gray-900 text-2xl font-extrabold uppercase md:text-3xl dark:text-white">KeHMIS HIS Products Demo, Training and Testing
-                <span class="underline text-blue-400">Environments</span></h1>
+            <h1 class="text-gray-900 text-2xl font-extrabold  md:text-3xl dark:text-white">KeHMIS HIS live, Training & Testing 
+                <span class="underline text-blue-400">Instances</span></h1>
                 <h2 class="text-green font-medium capitalize dark:text-white
-                text-gray-600 ">EMR DEMO AND TRAINING SERVERS</h2>
-            <p class="dark:text-slate-100 font-Medium text-green-900">These are the released versions of KeHMIS products. Use of these servers should be primarily for demo and training.</p>
-           <a href="#training-servers"> <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Learn More</button>
+                text-gray-600 ">List of all Servers</h2>
+            <p class="dark:text-slate-100 font-Medium text-green-900">These are the released versions of KeHMIS products. Use of these testing Instances should be primarily for demo and training.</p>
+           <a href="#training-servers"> <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Test Instances</button>
+           </a>
+           <a href="#prod-servers"> <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Production  Instances</button>
            </a></div>
     </div>
 </div>
